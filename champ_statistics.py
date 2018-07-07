a33ef3f88acc4aa78c20c729d539ace0347ea9e8
Skyler Daché <skyberskyman@gmail.com>
2018-07-07 02:56:15 -0400

    reordered and commented champ statistics

--- champ_statistics.py
+++ champ_statistics.py
@@ -1,33 +1,41 @@
 from enum import Enum, auto
+
+# statistics that can be modified by items, spell effects, buffs, or debuffs:
 class ChampStatistic(Enum):
-    # statistics that can be modified by items, spell effects, buffs, or debuffs
-    HP = auto()
-    HP_BONUS_PERCENT = auto()
-    MANA = auto()
-    MOVE_SPEED_FLAT = auto()
-    MOVE_SPEED_PERCENT = auto()
-    TENACITY = auto()
-    SLOW_RESIST = auto()
-    ARMOR = auto()
-    ARMOR_PERCENT = auto()
-    MAGIC_RESIST = auto()
-    ATTACK_RANGE = auto()
-    ATTACK_RANGE_PERCENT = auto()
-    HP_REGEN = auto()
-    HP_REGEN_PERCENT = auto()
-    MANA_REGEN = auto()
-    MANA_REGEN_PERCENT = auto()
-    CRIT_CHANCE = auto()
-    CRIT_DAMAGE = auto()
+    # Offensive, other than lifesteal/drain (see below):
     ATTACK_DAMAGE = auto()
+    ATTACK_DAMAGE_BONUS_PERCENT = auto()
     ATTACK_SPEED_PERCENT = auto()
     ABILITY_POWER = auto()
     ABILITY_POWER_PERCENT = auto()
-    HEAL_AND_SHIELD_POWER = auto()
+    CRIT_CHANCE = auto()
+    CRIT_DAMAGE = auto()
 
-    # _____Lifesteal and spell vamp___________
-    # Vamp and drain are distinct because drain benefits from heal power,
-    # while vamp does not
+    # Defensive:
+    HP = auto()
+    HP_BONUS_PERCENT = auto()
+    HP_REGEN = auto()
+    HP_REGEN_PERCENT = auto()
+    ARMOR = auto()
+    ARMOR_PERCENT = auto()
+    MAGIC_RESIST = auto()
+    TENACITY = auto()
+    SLOW_RESIST = auto()
+
+    # Utility:
+    COOLDOWN_REDUCTION = auto()
+    MANA = auto()
+    MANA_REGEN = auto()
+    MANA_REGEN_PERCENT = auto()
+    HEAL_AND_SHIELD_POWER = auto()
+    MOVE_SPEED_FLAT = auto()
+    MOVE_SPEED_PERCENT = auto()
+    ATTACK_RANGE = auto()
+    ATTACK_RANGE_PERCENT = auto()
+
+    # Lifesteal and spell vamp
+    ## (Vamp and drain are distinct because drain benefits from heal power,
+    ## while vamp does not)
     # Autoattacks:
     AUTOATTACK_VAMP = auto()
     AUTOATTACK_DRAIN = auto()
