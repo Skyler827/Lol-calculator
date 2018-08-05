f7b9848e2eb11700432f518b30318e2692465713
Skyler Daché <skyberskyman@gmail.com>
2018-08-04 23:02:41 -0400

    Abstracted out champ names, made basic_attack function return damage

--- champion.py
+++ champion.py
@@ -227,6 +227,7 @@ class Champion(AbstractMinion):
         hp_percent = self.hp / self.get_maxhp()
         self.level += 1
         self.hp = hp_percent * self.get_maxhp()
+    
     def take_damage(self, attack: Damage) -> None:
         if attack.type == DamageType.PHYSICAL:
             armor = self.get_armor()
@@ -237,6 +238,9 @@ class Champion(AbstractMinion):
             damage = (attack.amount) * (100/(armor+100))
             damage *= (1-self.damagereduction_percent/100)
             self.hp -=  damage
+            if self.hp <= 0:
+                self.die()
+            return damage
         elif attack.type == DamageType.MAGICAL:
             magic_resist = self.get_magic_resist()
             for i in attack.pen_magic_percent:
@@ -245,12 +249,14 @@ class Champion(AbstractMinion):
             damage = (attack.amount) * (100/magic_resist)
             damage *= (1-self.damagereduction_percent/100)
             self.hp -=  damage
+            if self.hp <= 0:
+                self.die()
+            return damage
         elif attack.type in DamageType.TRUE or DamageType.PURE:
             self.hp -= attack.amount
+            return damage
         else:
             raise Exception
-        if self.hp <= 0:
-            self.die()
     def basic_attack(self, target: AbstractMinion) -> None:
         attack = Damage(DamageType.PHYSICAL, amount=self.get_attackdamage(), \
             pen_armor_percent=self.pen_armor_percent, pen_lethality=self.pen_lethality)
@@ -258,10 +264,6 @@ class Champion(AbstractMinion):
         for effect in self.onHitEffects:
             target.take_damage(effect.getDamage(effect, target))
 
-    def buy_item(self, item):
-        pass
-    def sell_item(self, item):
-        pass
     def Spell1(self, target):
         pass
     def Spell2(self, target):
--- timer.py
+++ timer.py
@@ -1,52 +1,63 @@
 import champion as c
 
-
-
 def log_status(champ_1, champ_2, time_elapsed):
     print(f"{round(time_elapsed)} seconds passed, {champ_1.name} has {round(champ_1.hp)} HP, while {champ_2.name} has {round(champ_2.hp)} HP.")
-def main():
-    leona = c.Champion("Leona")
-    master_yi = c.Champion("Master Yi")
-    master_yi_must_wait = 0
-    leona_must_wait = 0
+def run_combat(blue_champion_name:str, red_champion_name:str):
+    blue_side_champ = c.Champion(blue_champion_name)
+    red_side_champ = c.Champion(red_champion_name)
+    red_champ_must_wait = 0
+    blue_champ_must_wait = 0
     time_elapsed = 0
-    while (leona.hp > 0 and master_yi.hp > 0):
-        log_status(leona, master_yi, time_elapsed)
-        if master_yi_must_wait < leona_must_wait:
-            # Time passes, then Yi attacks Leona
-            time_passes = master_yi_must_wait
-            master_yi_must_wait = 0
-            leona_must_wait -= time_passes
+    
+    initial = {
+        
+    }
+    events = []
+    
+    while (blue_side_champ.hp > 0 and red_side_champ.hp > 0):
+        log_status(blue_side_champ, red_side_champ, time_elapsed)
+        if red_champ_must_wait < blue_champ_must_wait:
+            # Time passes, then Red attacks Blue
+            time_passes = red_champ_must_wait
+            red_champ_must_wait = 0
+            blue_champ_must_wait -= time_passes
             time_elapsed += time_passes
-            print("Yi attacks!")
-            master_yi.basic_attack(leona)
-            master_yi_must_wait += master_yi.get_attack_time()
+            print(f"{red_side_champ.name} attacks!")
+            red_side_champ.basic_attack(blue_side_champ)
+            red_champ_must_wait += red_side_champ.get_attack_time()
 
-        elif leona_must_wait < master_yi_must_wait:
-            # Time passes, then Leona attacks Yi
-            time_passes = leona_must_wait
-            leona_must_wait = 0
-            master_yi_must_wait -= time_passes
+        elif blue_champ_must_wait < red_champ_must_wait:
+            # Time passes, then Blue attacks Red
+            time_passes = blue_champ_must_wait
+            blue_champ_must_wait = 0
+            red_champ_must_wait -= time_passes
             time_elapsed += time_passes
-            print("Leona Attacks!")
-            leona.basic_attack(master_yi)
-            leona_must_wait += leona.get_attack_time()
-        elif leona_must_wait == master_yi_must_wait == 0:
+            print(f"{blue_side_champ.name} Attacks!")
+            blue_side_champ.basic_attack(red_side_champ)
+            blue_champ_must_wait += blue_side_champ.get_attack_time()
+        elif blue_champ_must_wait == red_champ_must_wait == 0:
             #Both may attack, no time passes
-            print("Yi Attacks!")
-            master_yi.basic_attack(leona)
-            master_yi_must_wait += master_yi.get_attack_time()
-            print("Leona Attacks!")
-            leona.basic_attack(master_yi)
-            leona_must_wait += leona.get_attack_time()
-        elif leona_must_wait == master_yi_must_wait > 0:
-            master_yi_must_wait = 0
-            leona_must_wait = 0
+            print(f"{red_side_champ.name} Attacks!")
+            red_side_champ.basic_attack(blue_side_champ)
+            red_champ_must_wait += red_side_champ.get_attack_time()
+            print(f"{blue_side_champ.name} Attacks!")
+            blue_side_champ.basic_attack(red_side_champ)
+            blue_champ_must_wait += blue_side_champ.get_attack_time()
+        elif blue_champ_must_wait == red_champ_must_wait > 0:
+            red_champ_must_wait = 0
+            blue_champ_must_wait = 0
+            time_elapsed += blue_champ_must_wait
         else: raise Exception
-    if master_yi.hp > 0:
-        print(f"Master Yi won, with {master_yi.hp} HP to spare")
-    elif leona.hp > 0:
-        print(f"Leona won, with {leona.hp} HP to spare")
+    if red_side_champ.hp > 0:
+        print(f"Master Yi won, with {red_side_champ.hp} HP to spare")
+    elif blue_side_champ.hp > 0:
+        print(f"Leona won, with {blue_side_champ.hp} HP to spare")
     else:
         print("both of their HP went to 0 at the same time")
-main()
+    return {
+        "in": initial,
+        "events": events,
+        "winner": blue_side_champ.name,
+        "winner_hp":blue_side_champ.hp
+    }
+run_combat("Leona", "Master Yi")
