6972015f7dbdba7473736e259ce42cdfcf87bc7c
Skyler Daché <skyberskyman@gmail.com>
2018-08-22 20:11:34 -0400

    worked on summoner spells

--- apps/calc/static/calc/js/champ_select.js
+++ apps/calc/static/calc/js/champ_select.js
@@ -4,57 +4,68 @@
 // 3 makes selections in the dialogs select the champs and closes the dialogs
 
 $(function() {
-    // make elements dialogs:
-    $("#blue-champ-select").dialog({
-        title: "Select Blue Side Champion",
-        width: 0.7*window.innerWidth,
-        autoOpen: false,
-        modal: true,
-        buttons: {
-            Ok: function() {
-                $( this ).dialog( "close" );
-            }
+    function make_champ_select(color) {
+        var champ_select = document.createElement("div");
+        champ_select.id = color+"-champ-select";
+        champ_select.classList.add("my-modal");
+        champ_select.classList.add("champ-select-modal");
+        var p_tag = document.createElement("p");
+        p_tag.innerHTML = "Select a champion:";
+        champ_select.appendChild(p_tag);
+        //TODO: write a django view for getting all the champs in json format
+        //      then write an ajax request here to get all the champ names
+        var champs = ["Ashe", "Taric", "Lulu"];
+        for (let i=0; i< champs.length; i++) {
+            let div = document.createElement("div");
+            div.classList.add("selectable");
+            div.classList.add("selectable-champ");
+            let img = document.createElement("img");
+            img.src = "static/calc/img/champ_square/"+champs[i]+".png";
+            div.appendChild(img);
+            let champ_p_tag = document.createElement("p");
+            let a_tag = document.createElement("a");
+            a_tag.text = champs[i];
+            a_tag.appendChild(document.createElement("span"));
+            champ_p_tag.appendChild(a_tag);
+            div.appendChild(champ_p_tag);
+            champ_select.appendChild(div);
+
+            $(a_tag).click(function(_) {
+                let champ_path = "/static/calc/img/champ_square/"+champs[i]+".png";
+                $("."+color+"-champ .champ-icon").attr("src", champ_path);
+                $("."+color+"-champ .clickable-image-container div.middle").hide();
+                $("."+color+"-champ p.champ-name").text(champs[i]);
+                $(champ_select).dialog("close");
+            });
         }
-    });
-    $("#red-champ-select").dialog({
-        title: "Select Red Side Champion",
-        width: 0.7*window.innerWidth,
-        autoOpen: false,
-        modal: true,
-        buttons: {
-            Ok: function() {
-                $( this ).dialog( "close" );
+        var empty_div = document.createElement("div");
+        champ_select.appendChild(empty_div);
+        $(champ_select).dialog({
+            title: "Select "+color+" Side Champion",
+            width: 0.7 * window.innerWidth,
+            autoOpen: true,
+            modal: true,
+            buttons: {
+                Cancel: function() {
+                    $( this ).dialog( "close" );
+                }
             }
-        }
-    });
+        });
+        return champ_select;
+    }
+
     $(window).resize(function() {
         $("#red-champ-select").dialog("option", "width", 0.7*window.innerWidth);
         $("#blue-champ-select").dialog("option", "width", 0.7*window.innerWidth);
     });
     //make links open the dialogs:
     $(".blue-champ a.clickable-image-container").click(function(e) {
-        $("#blue-champ-select").dialog("open");
+        var champ_select = make_champ_select("blue");
+        document.appendChild(champ_select);
     });
     $(".red-champ a.clickable-image-container").click(function(e) {
-        $("#red-champ-select").dialog("open");
+        var champ_select = make_champ_select("red");
+        document.appendChild(champ_select);
     });
 
-    //make items in dialog set the champs and close the dialog
-    $("#blue-champ-select .selectable-champ a").click(function(e) {
-        let champName = e.currentTarget.innerText;
-        let champ_path = "/static/calc/img/champ_square/"+champName+".png";
-        $(".blue-champ .champ-icon").attr("src", champ_path);
-        $(".blue-champ .clickable-image-container div.middle").hide();
-        $(".blue-champ p.champ-name").text(champName);
-        $("#blue-champ-select").dialog("close")
-
-    });
-    $("#red-champ-select .selectable-champ a").click(function(e) {
-        let champName = e.currentTarget.innerText;
-        let champ_path = "/static/calc/img/champ_square/"+champName+".png";
-        $(".red-champ .champ-icon").attr("src", champ_path);
-        $(".red-champ .clickable-image-container div.middle").hide();
-        $(".red-champ p.champ-name").text(champName);
-        $("#red-champ-select").dialog("close")
-    });
 });
--- apps/calc/static/calc/js/summoner_select.js
+++ apps/calc/static/calc/js/summoner_select.js
@@ -1,3 +1,35 @@
 $(function() {
+    function make_summoner_spell_modal(color, key) {
+        // Define the modal
+        var summ_selector = document.createElement("div");
+        summ_selector.id = "summoner-spell-selector-"+color+"-"+key;
+        summ_selector.classList.add("my-modal");
+        summ_selector.classList.add("summoner-spell-modal");
+        var p_tag = document.createElement("p");
+        p_tag.innerHTML = "Select a champion:";
+        summ_selector.appendChild(p_tag);
+        var spell_names = [
+            "Barrier", "Exhaust", "Flash", "Heal", "Smite", "Teleport"];
+        for (let i=0; i<spell_names.length; i++) {
+            // Define the summoner spells
+            let div = document.createElement("div");
+            div.classList.add("selectable");
+            div.classList.add("selectable-summ");
+            let img = document.createElement("img");
+            img.src = "static/calc/img/summoner/Summoner"+spell_names[i]+".png";
+            div.appendChild(img);
+            let p_label = document.createElement("p");
+            let a_tag = document.createElement("a");
+            a_tag.text = spell_names[i];
+            a_tag.appendChild(document.createElement("span"));
+            p_label.appendChild(a_tag);
+            div.appendChild(champ_p_tag);
+            summ_selector.appendChild(div);
+
+            $(a_tag).click(function(_){
+                console.log("ayo");
+            });
+        }
+    }
     console.log("ayy");
 });
